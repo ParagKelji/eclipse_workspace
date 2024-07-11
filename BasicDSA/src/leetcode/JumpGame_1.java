e4340645ae8e295bb7fda0e1ce2dc9d90bfd5c67
Parag R Kelji <kelji.parag@gmail.com>
2024-07-10 21:28:42 -0700

    Tested and submitted JumpGame 1 to leetcode.

--- BasicDSA/src/leetcode/JumpGame_1.java
+++ BasicDSA/src/leetcode/JumpGame_1.java
@@ -1,52 +1,71 @@
 package leetcode;
 
+import java.util.Arrays;
+import java.util.function.IntPredicate;
+import java.util.stream.IntStream;
+
 public class JumpGame_1 {
 	
 	public boolean canJump(int[] nums) {
-        boolean isJumpable = false;
-        if ( nums == null || nums.length <= 0 ) {
+		boolean isJumpable = false;
+		if ( nums == null || nums.length <= 0 ) {
 			return isJumpable;
 		}
         if ( nums.length == 1 ) {
 			return true;
 		}
-        int end = nums.length - 1;
-        int firstJumpMax = nums[0];
-        if ( firstJumpMax <= 0 ) {
-			return isJumpable;
-		}
-        if ( firstJumpMax >= end ) {
-        	return true;
+        int len = nums.length;
+        boolean jumpedAtZero = false;
+        int jumpValToAvoidZero = 0;
+        nums[len - 1] += 1;
+        boolean containsZero = Arrays.stream(nums).anyMatch((num) -> num == 0 );
+        isJumpable = !containsZero;
+        if ( !isJumpable ) {
+        	for ( int i = len - 1; i >= 0; i-- ) {
+        		if ( !jumpedAtZero && nums[i] == 0 ) {
+        			jumpValToAvoidZero = 2;
+        			jumpedAtZero = true;
+        			continue;
+        		}
+        		if ( jumpedAtZero && nums[i] >= jumpValToAvoidZero ) {
+        			jumpedAtZero = false;
+        		} else {
+        			jumpValToAvoidZero++;
+        		}
+        	}
         }
-        int jumpDistance = 0;
-        for ( int i = 1; i <= firstJumpMax; i++ ) {
-        	jumpDistance = i;
-        	while ( jumpDistance < end ) {
-				int currJump = nums[jumpDistance];
-				if ( currJump == 0 ) {
-					break;
-				}
-        		jumpDistance += currJump;
-				if ( jumpDistance >= end ) {
-					isJumpable = true;
-					break;
-				}
-			}
-        	if ( isJumpable ) {
-				break;
-			}
+        nums[len - 1] += 1;
+        if ( jumpedAtZero ) {
+        	isJumpable = false;
+        } else {
+        	isJumpable = true;
         }
-        return isJumpable;
+		return isJumpable;
+	}
+	
+	public boolean canJump1(int[] nums) {
+		int reachable = 0;
+	       for(int i = 0; i < nums.length; i ++) {
+	           if(i > reachable) return false;
+	           reachable = Math.max(reachable, i + nums[i]);
+	       } 
+	       return true;
     }
 	
 	public static void main(String[] args) {
 		JumpGame_1 jmpGame = new JumpGame_1();
 		int[] nums = {3,2,1,0,4};
-		//System.out.println(jmpGame.canJump(nums));
+		System.out.println(jmpGame.canJump1(nums));
 		int[] nums2 = {2,3,1,1,4};
-		//System.out.println(jmpGame.canJump(nums2));
+		System.out.println(jmpGame.canJump1(nums2));
 		int[] nums3 = {1,2,3};
-		System.out.println(jmpGame.canJump(nums3));
+		System.out.println(jmpGame.canJump1(nums3));
+		int[] nums4 = {2,0};
+		System.out.println(jmpGame.canJump1(nums4));
+		int[] nums5 = {2,3,1,1,4};
+		System.out.println(jmpGame.canJump1(nums5));
+		int[] nums6 = {2,0,0};
+		System.out.println(jmpGame.canJump1(nums6));
 	}
 
 }
